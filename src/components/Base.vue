bb534eaa926cf2e9815d7a8fb90cf36419c5c931
TristianPaquette <155991405+TristianPaquette@users.noreply.github.com>
2025-10-21 20:37:17 -0400

    Update Base.vue

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="baseBeverage"></div>
+  <div class="baseBeverage" :style="{ backgroundColor: baseColor }"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+
+// receives which base drink was chosen
+const props = defineProps<{ type: string }>()
+
+// gives a simple color for each base drink
+const baseColor = computed(() => {
+  if (props.type === 'Coffee') return '#936c45ff'
+  if (props.type === 'Green Tea') return '#8fbc8f'
+  if (props.type === 'Black Tea') return '#452818ff'
+})
+</script>
 
 <style scoped>
 .baseBeverage {
@@ -12,6 +24,6 @@
   bottom: 0;
   animation: pour-tea 2s;
   z-index: 300;
-  /* // border-radius: 0.05em 0.05em 2.2em 2.2em; */
+  border-radius: 0.05em 0.05em 2.2em 2.2em;
 }
 </style>
